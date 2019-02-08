bca6695fdc130c7aefc87b1f8d5d35faf17e356e
Todd Rasband <todd.rasband@gmail.com>
2019-02-07 20:13:21 -0700

    Update logic.test.js

--- src/test/logic.test.js
+++ src/test/logic.test.js
@@ -12,8 +12,22 @@ describe('Tests Toggle Show button', () => {
 
 describe('Can add item to cart', () => {});
 
+describe('can remove item', () => {
+  // Using a beforeEach set a cart array to have 2 or more items of varrying quality
+  // can lower quantity by one if quantity is over 1
+  // can remove item from cart if quantity is 1
+  // returns a new array
+  // does not modify the items from the original cart
+  // can handle being given an empty array
+  // can handle if the item is not found in the array
+});
+
+// This function should take in one product item from a cart and return a string.
+// Its a string so that we can always display two decimal points
+// It should also display a dolar sign at the beggining
 describe('can calculate sub total', () => {
   // can get correct sub total
+  // can get a correct total with different input
   // has 2 decimal points at the end
   // does not go past 2 decimal palces
   // returns a string(because numbers can't be 1.00 it has to be a string to keep the two deciamals)
@@ -21,21 +35,15 @@ describe('can calculate sub total', () => {
   // does not modify passed in object
 });
 
+// This function should take in the array of products and return a string
+// Its a string so that we can always display two decimal points
+// It should also display a dolar sign at the beggining
 describe('can calculate Total', () => {
-  // can get correct sub total
-  // does have 2 decimal points at the end
+  // can get correct total
+  // can get a correct total with different input
+  // Have 2 decimal points at the end
   // does not go past 2 decimal palces
   // returns a string(because numbers can't be 1.00 it has to be a string to keep the two deciamals)
   // starts with a $
   // does not modify passed in array
-});
-
-describe('can remove item', () => {
-  // before each setUp a test cart with 2 items of varying quantity
-  // can lower quantity by one if quantity is over 1
-  // can remove item from cart if quantity is 1
-  // returns a new array
-  // does not modify the items from the original cart
-  // can handle being given an empty array
-  // can handle if the item is not found in the array
 });
